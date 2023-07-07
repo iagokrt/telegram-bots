22af1b8fca0815f8e74be16c5f483ddb8341811a
Iago Barreto <iago_floripa@hotmail.com>
2023-07-07 20:42:05 -0300

    add env file

--- songs_bot/gen_bot/gen_bot_ino.py
+++ songs_bot/gen_bot/gen_bot_ino.py
@@ -3,7 +3,6 @@ import textwrap
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from env import token
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,26 +11,8 @@ logging.basicConfig(
 
 def generate_balloon(text):
     lines = text.split("\n")
-    max_length = max(len(line) for line in lines)
-    balloon = []
-    balloon.append(" " + "_" * (max_length + 2))
-    for line in lines:
-        balloon.append("< {} >".format(line.ljust(max_length)))
-    balloon.append(" " + "-" * (max_length + 2))
-    return balloon
-
-def cowsay(text, eyes_option="sad"):
-    # eyes = get_eyes(eyes_option)
-    balloon = generate_balloon(text)
-    output = []
-    output.append("{}".format("".join(balloon)))
-    output.append("{}".format(r'        \   ^__^'))
-    output.append("{}".format(r'         \  (oo)\_______'))
-    # output.append("{}".format(r'         \  ({})\_______'.format(eyes)))
-    output.append("{}".format(r'            (__)\       )\/\\'))
-    output.append("{}".format(r'                ||----w |'))
-    output.append("{}".format(r'                ||     ||'))
-    return "\n".join(output)
+ 
+    return lines
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
@@ -39,7 +20,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     ## get input message from telegram user
     user_input = update.message.text
-    command = '/vacas'  # Change this to the appropriate command
+    command = '/gen'  # Change this to the appropriate command
     message_text = user_input.replace(command, '').strip()
 
     cow_message = cowsay(message_text)
@@ -53,7 +34,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(token).build()
+    application = ApplicationBuilder().token('6139722782:AAF5oXTywXGxEtBapHAC7mAtklCY-0I6nEc').build()
     
     start_handler = CommandHandler('vacas', start)
     application.add_handler(start_handler)
